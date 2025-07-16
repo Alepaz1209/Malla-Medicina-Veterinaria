38af69237e95df61d08feb3091e3e27954f75f95
Alepaz1209 <alejandraapaazz17@gmail.com>
2025-07-16 17:35:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,19 +29,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   updateSubjectStates(); // inicial
 });
-
-
-      const listaRequisitos = requisitos.split(",").map(r => r.trim());
-      const todosCumplidos = listaRequisitos.every(req => aprobados.has(req));
-
-      if (todosCumplidos) {
-        ramo.classList.remove("bloqueado");
-      } else {
-        ramo.classList.add("bloqueado");
-        ramo.classList.remove("aprobado");
-        aprobados.delete(ramo.id);
-        localStorage.setItem("aprobados", JSON.stringify([...aprobados]));
-      }
-    });
-  }
-});
